0e6323b515b97fc04523e4cb11af720586bd63e8
Dirley Pinto <dirley.figueredo@gmail.com>
2021-11-04 23:38:13 -0300

    Spring Data JPQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import com.devsuperior.uri2602.repositories.CustomerRepository;
 
@@ -17,17 +18,34 @@ public class Uri2602Application implements CommandLineRunner {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<CustomerMinProjection> list = customerRepository.search1("RS");
-		List<CustomerMinDTO> dtos = list.stream().map(x -> new CustomerMinDTO((x))).collect(Collectors.toList());
+		List<CustomerMinProjection> result1 = customerRepository.search1("RS");
+		List<CustomerMinDTO> dtos = result1.stream().map(x -> new CustomerMinDTO((x))).collect(Collectors.toList());
+		System.out.println("RESULTADO SQL RAIZ:");
 		for (CustomerMinDTO dto : dtos) {
 			System.out.println(dto.getName());
+		}
+
+		System.out.println("\n");
+
+		List<CustomerMinDTO> result2 = customerRepository.search2("RS");
+		System.out.println("RESULTADO JPQL DTO:");
+		for (CustomerMinDTO dto : result2) {
+			System.out.println(dto.getName());
+		}
+
+		System.out.println("\n");
+
+		List<Customer> result3 = customerRepository.search3("RS");
+		System.out.println("RESULTADO JPQL ENTITY:");
+		for (Customer entity : result3) {
+			System.out.println(entity.getName());
 		}
 	}
 }
--- uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerMinProjection;
 
@@ -12,4 +13,10 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
 	@Query(nativeQuery = true, value = "SELECT NAME FROM CUSTOMERS C WHERE C.STATE = :state")
 	List<CustomerMinProjection> search1(String state);
+
+	@Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(obj.name) FROM Customer obj WHERE obj.state = :state")
+	List<CustomerMinDTO> search2(String state);
+
+	@Query("SELECT obj FROM Customer obj WHERE obj.state = :state")
+	List<Customer> search3(String state);
 }
