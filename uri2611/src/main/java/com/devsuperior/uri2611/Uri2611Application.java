1fe5eed04b003e00c5252a513d91550990b4b235
Dirley Pinto <dirley.figueredo@gmail.com>
2021-11-06 08:40:48 -0300

    Spring Data Projection and JPQL using JOIN

--- uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,12 +1,42 @@
 package com.devsuperior.uri2611;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2611.dto.MovieMinDTO;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+
 @SpringBootApplication
-public class Uri2611Application {
-	
+public class Uri2611Application implements CommandLineRunner {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<MovieMinProjection> result1 = movieRepository.search1("Action");
+		List<MovieMinDTO> dtos = result1.stream().map(x -> new MovieMinDTO((x))).collect(Collectors.toList());
+		System.out.println("RESULTADO SQL RAIZ:");
+		for (MovieMinDTO dto : dtos) {
+			System.out.println(dto.getName());
+		}
+
+		System.out.println("\n");
+
+		List<MovieMinDTO> result2 = movieRepository.search2("Action");
+		System.out.println("RESULTADO JPQL DTO:");
+		for (MovieMinDTO dto : result2) {
+			System.out.println(dto.getName());
+		}
 	}
 }
---
+++ uri2611/src/main/java/com/devsuperior/uri2611/dto/MovieMinDTO.java
@@ -0,0 +1,39 @@
+package com.devsuperior.uri2611.dto;
+
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+
+public class MovieMinDTO {
+
+	private Long id;
+	private String name;
+	
+	public MovieMinDTO() {
+	}
+
+	public MovieMinDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public MovieMinDTO(MovieMinProjection cmp) {
+		this.id = cmp.getId();
+		this.name = cmp.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
